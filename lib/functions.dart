b65160e1280dd81a8b8548d56f9f12bfbeebc5e0
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-03-20 15:33:34 -0300

    Projeto base para micro service

---
+++ lib/functions.dart
@@ -0,0 +1,12 @@
+import 'package:functions_framework/functions_framework.dart';
+import 'package:shelf/shelf.dart';
+
+@CloudFunction()
+Response function(Request request) {
+  return Response.ok('Hello, World!');
+}
+
+@CloudFunction()
+Response teste(Request request) {
+  return Response.ok('teste');
+}
